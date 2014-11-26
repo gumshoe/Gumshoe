7cab31b2949c4435bf04e17be4b42e9210b6fb6c
shellscape <andrew@shellscape.org>
2014-11-26 13:44:10 -0500

    moving performance.now into lib

--- gumeshoe.js
+++ gumeshoe.js
@@ -14,28 +14,6 @@
 
 function (_) {
 
-  /**
-   * @file perfnow is a 0.14 kb window.performance.now high resolution timer polyfill with Date fallback
-   * @author Daniel Lamb <dlamb.open.source@gmail.com>
-   */
-  (function perfnow (window) {
-    // make sure we have an object to work with
-    if (!('performance' in window)) {
-      window.performance = {};
-    }
-    var perf = window.performance;
-    // handle vendor prefixing
-    window.performance.now = perf.now ||
-      perf.mozNow ||
-      perf.msNow ||
-      perf.oNow ||
-      perf.webkitNow ||
-      // fallback to Date
-      Date.now || function () {
-        return new Date().getTime();
-      };
-  })(window);
-
   var defaults = {
       transport: '',
     },
@@ -62,7 +40,6 @@ function (_) {
       throw 'Gumeshoe: Transport property must be a [String] or [Array].'
     }
   }
-
 
   function collect () {
     var result = {
